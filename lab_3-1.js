7368b89a316ba15877923bc49c32b559604ab77a
p22crsullivan <p22crsullivan@fairfieldprep.org>
2021-02-11 11:34:02 -0500

    Submit Cycle 3 Day 3 Labs

---
+++ lab_3-1.js
@@ -0,0 +1,3 @@
+// Author: CRS 2/11/2021
+var myLevel = 6;
+if (myLevel % 2 == 0) {console.log(myLevel + " is an even number.");}
---
+++ lab_3-2.js
@@ -0,0 +1,4 @@
+// Author: CRS 2/11/2021
+var myLevel = 5;
+if (myLevel % 2 == 0) {console.log(myLevel + " is an even number.");}
+else {console.log(myLevel + " is an odd number.");}
