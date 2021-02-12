d1af0c566d71dfe7900396f1443ab91b9680a879
p22crsullivan <p22crsullivan@fairfieldprep.org>
2021-02-12 13:52:49 -0500

    Fixed assignments in wrong folders

--- lab_2-1.js
+++
@@ -1,7 +0,0 @@
-// Author CRS 2/10/2021
-var playerName = "Christopher Sullivan";
-console.log(playerName.slice(0, 11));
-console.log(playerName.slice(12, 20));
-var firstName = playerName.slice(0, 11);
-var lastName = playerName.slice(12, 20);
-console.log(firstName + lastName);
--- lab_2-2.js
+++
@@ -1,5 +0,0 @@
-// Author: CRS 2/10/2021
-var schoolName = "Fairfield Prep";
-console.log("Fairfield Prep"[4]);
-console.log("Fairfield Prep".slice(4, 5));
-console.log("Fairfield Prep".charAt(4));
