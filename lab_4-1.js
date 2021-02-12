a5af944d3ce8cd9d9978cc077985719a97dcd58e
p22crsullivan <p22crsullivan@fairfieldprep.org>
2021-02-12 14:30:51 -0500

    Lab 4-1 and 4-2 submissions

---
+++ lab_4-1.js
@@ -0,0 +1,7 @@
+// Author: CRS 2/12/2021
+var points = 8;
+if (points >= 15) {console.log("You win a gold medal!");}
+else if (points >= 12) {console.log("You win a silver medal!");}
+else if (points >= 9) {console.log("You win a silver medal!");}
+else if (points)
+else {console.log("You lose.");}
---
+++ lab_4-2.js
@@ -0,0 +1,5 @@
+// Author: CRS 2/12/2021
+var strength = 95;
+var shield = 80;
+if (strength >= 90 && shield >= 75) {console.log("You defeated the dragon!");}
+else ()
